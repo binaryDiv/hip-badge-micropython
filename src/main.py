9e10c16391ef9102fad7c2da1c86f7d212d3f7f9
binaryDiv <lexi@binarydiv.dev>
2022-12-29 20:21:03 +0100

    Implement a green moving dot for the default app

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 
 from machine import Pin
 from neopixel import NeoPixel
+import time
 
 # -- Define constants
 
@@ -13,12 +14,22 @@ LED_TOTAL_COUNT = 16
 
 # -- Setup device
 
-# Setup the Neopixel library, which is an easy way to control the LEDs.
+# Setup the NeoPixel library, which is an easy way to control the LEDs.
 led_pin = Pin(LED_DATA_PIN_NUMBER, Pin.OUT)
 neopixel = NeoPixel(led_pin, LED_TOTAL_COUNT)
 
-# For now, just set some static color for the LEDs and exit. (The LEDs will stay
-# on unless they are explicitly deactivated.)
-color = (32, 0, 0)  # RGB values from 0 to 255 as a tuple
-neopixel.fill(color)
-neopixel.write()
+# This will create a green dot that is moving in a circle. All other pixels will be red.
+# (We set the color values to 32 instead of 255, which would be "full brightness", because the LEDs are *very* bright.)
+background_color = (32, 0, 0)
+dot_color = (0, 32, 0)
+
+while True:
+    for i in range(LED_TOTAL_COUNT):
+        # Set all pixels to the background color
+        neopixel.fill(background_color)
+        # Set the moving pixel to a different color
+        neopixel[i] = dot_color
+        # Send the data to the LEDs
+        neopixel.write()
+        # Sleep for 0.333 milliseconds
+        time.sleep(1 / 3)
