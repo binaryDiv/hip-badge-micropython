e383ad9d424a8dc090e198fd562acb163dddeace
dysphoricUnicorn <git@dysphoric.dev>
2022-12-31 19:12:55 +0100

    Fix inconsistent var naming and move button definitions further up

--- examples/patterns.py
+++ examples/patterns.py
@@ -25,6 +25,10 @@ LED_TOTAL_COUNT = 16
 led_pin = Pin(LED_DATA_PIN_NUMBER, Pin.OUT)
 neopixel = NeoPixel(led_pin, LED_TOTAL_COUNT)
 
+gray_button = Pin(2, Pin.IN)
+red_button = Pin(8, Pin.IN)
+black_button = Pin(9, Pin.IN)
+
 # Define an array of colors
 color_combinations = [
     # Transgender pride flag
@@ -70,11 +74,11 @@ mutable_state = {
     'brightness_index': 1,
     'color_index': 0,
     'animate': False,
-    'animationFrame': 0,
+    'animation_frame': 0,
     'reverse': False,
-    'animateBrightness': False,
+    'animate_brightness': False,
     'tick': 0,
-    'animationSlowdown': 1,
+    'animation_slowdown': 1,
 }
 
 
@@ -85,17 +89,17 @@ def set_colors():
     # Loop through all neopixels and set their colors
     color_index = 0
     if mutable_state['animate']:
-        color_index = mutable_state['animationFrame']
+        color_index = mutable_state['animation_frame']
         if mutable_state['reverse']:
-            if mutable_state['animationFrame'] < len(current_colors) - 1:
-                mutable_state['animationFrame'] += 1
+            if mutable_state['animation_frame'] < len(current_colors) - 1:
+                mutable_state['animation_frame'] += 1
             else:
-                mutable_state['animationFrame'] = 0
+                mutable_state['animation_frame'] = 0
         else:
-            if mutable_state['animationFrame'] > 0:
-                mutable_state['animationFrame'] -= 1
+            if mutable_state['animation_frame'] > 0:
+                mutable_state['animation_frame'] -= 1
             else:
-                mutable_state['animationFrame'] = len(current_colors) - 1
+                mutable_state['animation_frame'] = len(current_colors) - 1
 
     for i in range(0, len(neopixel)):
         neopixel[i] = current_colors[color_index]
@@ -118,24 +122,24 @@ def read_pins():
     gray_button_value = gray_button.value()
     red_button_value = red_button.value()
     if black_button_value == 0 and red_button_value == 0:
-        mutable_state['animateBrightness'] = not mutable_state['animateBrightness']
+        mutable_state['animate_brightness'] = not mutable_state['animate_brightness']
     elif red_button_value == 0 and gray_button_value == 0:
-        if mutable_state['animationSlowdown'] < 3:
-            mutable_state['animationSlowdown'] += 1
+        if mutable_state['animation_slowdown'] < 3:
+            mutable_state['animation_slowdown'] += 1
         else:
-            mutable_state['animationSlowdown'] = 1
+            mutable_state['animation_slowdown'] = 1
     elif red_button_value == 0:
         if mutable_state['animate']:
             mutable_state['animate'] = False
             mutable_state['reverse'] = not mutable_state['reverse']
         else:
             mutable_state['animate'] = True
-            mutable_state['animationFrame'] = 0
+            mutable_state['animation_frame'] = 0
     elif black_button_value == 0:
-        mutable_state['animateBrightness'] = False
+        mutable_state['animate_brightness'] = False
         update_brightness()
     elif gray_button_value == 0:
-        mutable_state['animationFrame'] = 0
+        mutable_state['animation_frame'] = 0
         if mutable_state['color_index'] < len(color_combinations) - 1:
             mutable_state['color_index'] += 1
         else:
@@ -148,16 +152,12 @@ def read_pins():
 
 set_colors()
 
-gray_button = Pin(2, Pin.IN)
-red_button = Pin(8, Pin.IN)
-black_button = Pin(9, Pin.IN)
-
 while True:
     if read_pins():
         set_colors()
-    # Only change colors every animationSlowdown ticks
-    if mutable_state['tick'] % mutable_state['animationSlowdown'] == 0:
-        if mutable_state['animateBrightness']:
+    # Only change colors every animation_slowdown ticks
+    if mutable_state['tick'] % mutable_state['animation_slowdown'] == 0:
+        if mutable_state['animate_brightness']:
             update_brightness(True)
         set_colors()
     # Count up the tick counter until it reaches 9999, once it does, jump back to 1. This is done to prevent overflows after long runtimes.
